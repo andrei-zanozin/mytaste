f1414426fc6cd63f34c872d449a3a73739734d7d
zanozin <very.good.username@gmail.com>
2021-03-24 01:05:37 +0400

    Initial.

---
+++ src/test/java/com/zanozin/sb/SbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zanozin.sb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
