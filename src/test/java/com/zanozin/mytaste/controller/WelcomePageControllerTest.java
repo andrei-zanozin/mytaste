20e7ac6f62038d38c426067ec0439bfcfa612d87
zanozin <very.good.username@gmail.com>
2021-08-17 23:37:40 +0400

    Used WebMvcConfigurer instead of welcome page controller.

--- src/test/java/com/zanozin/mytaste/controller/WelcomePageControllerTest.java
+++ src/test/java/com/zanozin/mytaste/controller/WelcomePageControllerTest.java
@@ -1,21 +1,21 @@
 package com.zanozin.mytaste.controller;
 
-import static org.hamcrest.Matchers.containsString;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
-
 import lombok.Setter;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 @RunWith(SpringRunner.class)
-@WebMvcTest(WelcomePageController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class WelcomePageControllerTest {
 
     @Setter(onMethod_ = {@Autowired})
